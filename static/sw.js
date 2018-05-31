ab03230589d86ea82788c406c237f2bd952383f9
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2018-05-31 23:53:50 +0800

    Remove Workbox getting started message from sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-if (workbox) {
-  console.log('Yay! Workbox is loaded 🎉');
-} else {
-  console.log("Boo! Workbox didn't load 😬");
-}
-
 workbox.routing.registerRoute(
     new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
     workbox.strategies.cacheFirst({
